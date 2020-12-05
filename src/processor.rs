e46c137b08db018e7f00005b5a45066de6aedbab
Evgeniy Terekhin <e_terekhin@wargaming.net>
2020-12-05 12:14:26 +0200

    parse all games (refactoring needed)

--- src/processor.rs
+++ src/processor.rs
@@ -1,16 +1,10 @@
 use std::str::FromStr;
 
 use crate::errors::Error;
-use crate::types::{Board, Game};
+use crate::types::Game;
 
 pub fn process(line: &str) -> Result<(), Error> {
-    let mut tokens: Vec<&str> = line.split(' ').collect();
-
-    let game = Game::from_str(tokens.remove(0))?;
+    let game = Game::from_str(line)?;
     println!("GAME -> {:?}", game);
-    let board = Board::from_str(tokens.remove(0))?;
-    println!("BOARD -> {:?}", board);
-    let raw_hands: Vec<&str> = tokens;
-
     Ok(())
 }
--- src/types.rs
+++ src/types.rs
@@ -6,19 +6,45 @@ use crate::errors::Error;
 
 #[derive(Debug)]
 pub enum Game {
-    TexasHoldem,
-    OmahaHoldem,
-    FiveCardDraw,
+    TexasHoldem(Board, Vec<THHand>),
+    OmahaHoldem(Board, Vec<OHHand>),
+    FiveCardDraw(Vec<FCDHand>),
 }
 
 impl FromStr for Game {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "texas-holdem" => Ok(Self::TexasHoldem),
-            "omaha-holdem" => Ok(Self::OmahaHoldem),
-            "five-card-draw" => Ok(Self::FiveCardDraw),
+        let mut tokens = s.split(' ').collect::<Vec<_>>();
+
+        let raw_game = tokens.remove(0);
+
+        match raw_game {
+            "texas-holdem" => {
+                let raw_board = tokens.remove(0);
+                let board = Board::from_str(raw_board)?;
+                let hands = tokens
+                    .into_iter()
+                    .map(THHand::from_str)
+                    .collect::<Result<Vec<THHand>, Error>>()?;
+                Ok(Self::TexasHoldem(board, hands))
+            }
+            "omaha-holdem" => {
+                let raw_board = tokens.remove(0);
+                let board = Board::from_str(raw_board)?;
+                let hands = tokens
+                    .into_iter()
+                    .map(OHHand::from_str)
+                    .collect::<Result<Vec<OHHand>, Error>>()?;
+                Ok(Self::OmahaHoldem(board, hands))
+            }
+            "five-card-draw" => {
+                let hands = tokens
+                    .into_iter()
+                    .map(FCDHand::from_str)
+                    .collect::<Result<Vec<FCDHand>, Error>>()?;
+                Ok(Self::FiveCardDraw(hands))
+            }
             _ => Err(Error::GameParseError(s.to_string())),
         }
     }
@@ -76,7 +102,7 @@ impl FromStr for Rank {
             "7" => Ok(Self::Seven),
             "8" => Ok(Self::Eight),
             "9" => Ok(Self::Nine),
-            "10" => Ok(Self::Ten),
+            "T" => Ok(Self::Ten),
             "J" => Ok(Self::Jack),
             "Q" => Ok(Self::Queen),
             "K" => Ok(Self::King),
@@ -92,11 +118,48 @@ pub struct Card(Rank, Suite);
 #[derive(Debug)]
 pub struct Board([Card; 5]);
 
+#[derive(Debug)]
+pub struct THHand([Card; 2]);
+
+#[derive(Debug)]
+pub struct OHHand([Card; 4]);
+
+#[derive(Debug)]
+pub struct FCDHand([Card; 5]);
+
+impl FromStr for THHand {
+    type Err = Error;
+
+    fn from_str(board: &str) -> Result<Self, Self::Err> {
+        let re = Regex::new(r"([2-9TJQKA])([cdhs])").unwrap();
+        let cards = re
+            .find_iter(board)
+            .map(|card| card.as_str())
+            .map(|card| {
+                let captures = re.captures(card).unwrap();
+
+                let rank = captures.get(1).unwrap().as_str();
+                let rank = Rank::from_str(rank)?;
+
+                let suite = captures.get(2).unwrap().as_str();
+                let suite = Suite::from_str(suite)?;
+
+                Ok(Card(rank, suite))
+            })
+            .collect::<Result<Vec<Card>, Error>>()?;
+
+        Ok(Self([
+            cards.get(0).unwrap().clone(),
+            cards.get(1).unwrap().clone(),
+        ]))
+    }
+}
+
 impl FromStr for Board {
     type Err = Error;
 
     fn from_str(board: &str) -> Result<Self, Self::Err> {
-        let re = Regex::new(r"([2-9]|10|[AJQK])([cdhs])").unwrap();
+        let re = Regex::new(r"([2-9TJQKA])([cdhs])").unwrap();
         let cards = re
             .find_iter(board)
             .map(|card| card.as_str())
@@ -119,6 +182,67 @@ impl FromStr for Board {
             cards.get(2).unwrap().clone(),
             cards.get(3).unwrap().clone(),
             cards.get(4).unwrap().clone(),
+        ]))
+    }
+}
+
+impl FromStr for OHHand {
+    type Err = Error;
+
+    fn from_str(board: &str) -> Result<Self, Self::Err> {
+        let re = Regex::new(r"([2-9TJQKA])([cdhs])").unwrap();
+        let cards = re
+            .find_iter(board)
+            .map(|card| card.as_str())
+            .map(|card| {
+                let captures = re.captures(card).unwrap();
+
+                let rank = captures.get(1).unwrap().as_str();
+                let rank = Rank::from_str(rank)?;
+
+                let suite = captures.get(2).unwrap().as_str();
+                let suite = Suite::from_str(suite)?;
+
+                Ok(Card(rank, suite))
+            })
+            .collect::<Result<Vec<Card>, Error>>()?;
+
+        Ok(Self([
+            cards.get(0).unwrap().clone(),
+            cards.get(1).unwrap().clone(),
+            cards.get(2).unwrap().clone(),
+            cards.get(3).unwrap().clone(),
+        ]))
+    }
+}
+
+impl FromStr for FCDHand {
+    type Err = Error;
+
+    fn from_str(board: &str) -> Result<Self, Self::Err> {
+        let re = Regex::new(r"([2-9TJQKA])([cdhs])").unwrap();
+        let cards = re
+            .find_iter(board)
+            .map(|card| card.as_str())
+            .map(|card| {
+                let captures = re.captures(card).unwrap();
+
+                let rank = captures.get(1).unwrap().as_str();
+                let rank = Rank::from_str(rank)?;
+
+                let suite = captures.get(2).unwrap().as_str();
+                let suite = Suite::from_str(suite)?;
+
+                Ok(Card(rank, suite))
+            })
+            .collect::<Result<Vec<Card>, Error>>()?;
+
+        Ok(Self([
+            cards.get(0).unwrap().clone(),
+            cards.get(1).unwrap().clone(),
+            cards.get(2).unwrap().clone(),
+            cards.get(3).unwrap().clone(),
+            cards.get(3).unwrap().clone(),
         ]))
     }
 }
