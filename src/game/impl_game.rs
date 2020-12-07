605b7a2d31170b0a671ec1c45fdc27538fe1e6b4
Evgeniy Terekhin <e_terekhin@wargaming.net>
2020-12-07 17:52:28 +0200

    add texas/omaha sorting skeleton

--- src/game/impl_game.rs
+++ src/game/impl_game.rs
@@ -1,16 +1,37 @@
 use crate::card::Card;
 use crate::combination::Combination;
+use crate::game::Board;
 use crate::game::Game;
+use crate::game::HandOf2;
+use crate::game::HandOf4;
 use crate::game::Variant;
 
 impl Game {
     pub fn ordered_hands(&self) -> Vec<Vec<Card>> {
         match self {
-            Self::TexasHoldem(_board, _hands) => {
-                unimplemented!()
+            Self::TexasHoldem(board, hands) => {
+                let mut tmp = hands
+                    .iter()
+                    .map(|hand| (hand, Self::texas_holdem_combination(*board, *hand)))
+                    .collect::<Vec<_>>();
+
+                tmp.sort_by(|(_, comb_a), (_, comb_b)| comb_a.partial_cmp(comb_b).unwrap());
+
+                tmp.into_iter()
+                    .map(|(hand, _combination)| hand.0.to_vec())
+                    .collect::<Vec<_>>()
             }
-            Self::OmahaHoldem(_board, _hands) => {
-                unimplemented!()
+            Self::OmahaHoldem(board, hands) => {
+                let mut tmp = hands
+                    .iter()
+                    .map(|hand| (hand, Self::omaha_holdem_combination(*board, *hand)))
+                    .collect::<Vec<_>>();
+
+                tmp.sort_by(|(_, comb_a), (_, comb_b)| comb_a.partial_cmp(comb_b).unwrap());
+
+                tmp.into_iter()
+                    .map(|(hand, _combination)| hand.0.to_vec())
+                    .collect::<Vec<_>>()
             }
             Self::FiveCardDraw(hands) => {
                 let mut tmp = hands
@@ -26,5 +47,13 @@ impl Game {
                     .collect::<Vec<_>>()
             }
         }
+    }
+
+    fn texas_holdem_combination(_board: Board, _hand: HandOf2) -> Combination {
+        unimplemented!()
+    }
+
+    fn omaha_holdem_combination(_board: Board, _hand: HandOf4) -> Combination {
+        unimplemented!()
     }
 }
