55bff75b97e3fb6f8a9738020c60f6fd3f904046
Evgeniy Terekhin <e_terekhin@wargaming.net>
2021-01-04 18:21:05 +0300

    add tests for impl_eq

--- src/combination/impl_eq.rs
+++ src/combination/impl_eq.rs
@@ -130,4 +130,669 @@ fn compare_extra(xs: &[Rank], ys: &[Rank]) -> bool {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::card::Rank;
+    use crate::combination::Combination;
+
+    #[test]
+    fn test_compare_high_card_with_different_combination() {
+        let lhs = Combination::HighCard { rank: Rank::Two };
+
+        assert_ne!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four, Rank::Five]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::TwoPairs {
+                low: Rank::Two,
+                high: Rank::Three,
+                extra: [Rank::Four]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::ThreeOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four]
+            }
+        );
+        assert_ne!(lhs, Combination::Straight { rank: Rank::Two });
+        assert_ne!(lhs, Combination::Flush { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::FullHouse {
+                two: Rank::Two,
+                three: Rank::Three
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::FourOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three]
+            }
+        );
+        assert_ne!(lhs, Combination::StraightFlush { rank: Rank::Two });
+    }
+
+    #[test]
+    fn test_compare_high_card_with_high_card() {
+        let lhs = Combination::HighCard { rank: Rank::Two };
+
+        assert_eq!(lhs, Combination::HighCard { rank: Rank::Two });
+        assert_ne!(lhs, Combination::HighCard { rank: Rank::Three });
+    }
+
+    #[test]
+    fn test_compare_pair_with_different_combination() {
+        let lhs = Combination::Pair {
+            rank: Rank::Two,
+            extra: [Rank::Three, Rank::Four, Rank::Five],
+        };
+
+        assert_ne!(lhs, Combination::HighCard { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::TwoPairs {
+                low: Rank::Two,
+                high: Rank::Three,
+                extra: [Rank::Four]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::ThreeOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four]
+            }
+        );
+        assert_ne!(lhs, Combination::Straight { rank: Rank::Two });
+        assert_ne!(lhs, Combination::Flush { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::FullHouse {
+                two: Rank::Two,
+                three: Rank::Three
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::FourOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three]
+            }
+        );
+        assert_ne!(lhs, Combination::StraightFlush { rank: Rank::Two });
+    }
+
+    #[test]
+    fn test_compare_pair_with_pair() {
+        let lhs = Combination::Pair {
+            rank: Rank::Two,
+            extra: [Rank::Three, Rank::Four, Rank::Five],
+        };
+
+        assert_eq!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four, Rank::Five]
+            }
+        );
+        assert_eq!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Five, Rank::Four]
+            }
+        );
+        assert_eq!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Two,
+                extra: [Rank::Five, Rank::Three, Rank::Four]
+            }
+        );
+        assert_eq!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Two,
+                extra: [Rank::Four, Rank::Three, Rank::Five]
+            }
+        );
+        assert_eq!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Two,
+                extra: [Rank::Five, Rank::Four, Rank::Three]
+            }
+        );
+        assert_eq!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Two,
+                extra: [Rank::Four, Rank::Five, Rank::Three]
+            }
+        );
+
+        assert_ne!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Three,
+                extra: [Rank::Four, Rank::Five, Rank::Three]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Three,
+                extra: [Rank::Jack, Rank::Five, Rank::Three]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Three,
+                extra: [Rank::Jack, Rank::Queen, Rank::Three]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Three,
+                extra: [Rank::Jack, Rank::Queen, Rank::King]
+            }
+        );
+    }
+
+    #[test]
+    fn test_compare_two_pairs_with_different_combination() {
+        let lhs = Combination::TwoPairs {
+            low: Rank::Two,
+            high: Rank::Three,
+            extra: [Rank::Four],
+        };
+
+        assert_ne!(lhs, Combination::HighCard { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four, Rank::Five]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::ThreeOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four]
+            }
+        );
+        assert_ne!(lhs, Combination::Straight { rank: Rank::Two });
+        assert_ne!(lhs, Combination::Flush { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::FullHouse {
+                two: Rank::Two,
+                three: Rank::Three
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::FourOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three]
+            }
+        );
+        assert_ne!(lhs, Combination::StraightFlush { rank: Rank::Two });
+    }
+
+    #[test]
+    fn test_compare_two_pairs_with_two_pairs() {
+        let lhs = Combination::TwoPairs {
+            low: Rank::Three,
+            high: Rank::Five,
+            extra: [Rank::Four],
+        };
+
+        assert_eq!(
+            lhs,
+            Combination::TwoPairs {
+                low: Rank::Three,
+                high: Rank::Five,
+                extra: [Rank::Four],
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::TwoPairs {
+                low: Rank::Two,
+                high: Rank::Five,
+                extra: [Rank::Four],
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::TwoPairs {
+                low: Rank::Three,
+                high: Rank::Six,
+                extra: [Rank::Four],
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::TwoPairs {
+                low: Rank::Three,
+                high: Rank::Five,
+                extra: [Rank::Six],
+            }
+        );
+    }
+
+    #[test]
+    fn test_compare_three_of_a_kind_with_different_combination() {
+        let lhs = Combination::ThreeOfAKind {
+            rank: Rank::Two,
+            extra: [Rank::Three, Rank::Four],
+        };
+
+        assert_ne!(lhs, Combination::HighCard { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four, Rank::Five]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::TwoPairs {
+                low: Rank::Two,
+                high: Rank::Three,
+                extra: [Rank::Four]
+            }
+        );
+        assert_ne!(lhs, Combination::Straight { rank: Rank::Two });
+        assert_ne!(lhs, Combination::Flush { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::FullHouse {
+                two: Rank::Two,
+                three: Rank::Three
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::FourOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three]
+            }
+        );
+        assert_ne!(lhs, Combination::StraightFlush { rank: Rank::Two });
+    }
+
+    #[test]
+    fn test_compare_three_of_a_kind_with_three_of_a_kind() {
+        let lhs = Combination::ThreeOfAKind {
+            rank: Rank::Two,
+            extra: [Rank::Three, Rank::Four],
+        };
+
+        assert_eq!(
+            lhs,
+            Combination::ThreeOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four],
+            }
+        );
+        assert_eq!(
+            lhs,
+            Combination::ThreeOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Four, Rank::Three],
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::ThreeOfAKind {
+                rank: Rank::Three,
+                extra: [Rank::Four, Rank::Three],
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::ThreeOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Five, Rank::Three],
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::ThreeOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Five, Rank::Six],
+            }
+        );
+    }
+
+    #[test]
+    fn test_compare_straight_with_different_combination() {
+        let lhs = Combination::Straight { rank: Rank::Two };
+
+        assert_ne!(lhs, Combination::HighCard { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four, Rank::Five]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::TwoPairs {
+                low: Rank::Two,
+                high: Rank::Three,
+                extra: [Rank::Four]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::ThreeOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four]
+            }
+        );
+        assert_ne!(lhs, Combination::Flush { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::FullHouse {
+                two: Rank::Two,
+                three: Rank::Three
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::FourOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three]
+            }
+        );
+        assert_ne!(lhs, Combination::StraightFlush { rank: Rank::Two });
+    }
+
+    #[test]
+    fn test_compare_straight_with_straight() {
+        let lhs = Combination::Straight { rank: Rank::Two };
+
+        assert_eq!(lhs, Combination::Straight { rank: Rank::Two });
+        assert_ne!(lhs, Combination::Straight { rank: Rank::Three });
+    }
+
+    #[test]
+    fn test_compare_flush_with_different_combination() {
+        let lhs = Combination::Flush { rank: Rank::Two };
+
+        assert_ne!(lhs, Combination::HighCard { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four, Rank::Five]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::TwoPairs {
+                low: Rank::Two,
+                high: Rank::Three,
+                extra: [Rank::Four]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::ThreeOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four]
+            }
+        );
+        assert_ne!(lhs, Combination::Straight { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::FullHouse {
+                two: Rank::Two,
+                three: Rank::Three
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::FourOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three]
+            }
+        );
+        assert_ne!(lhs, Combination::StraightFlush { rank: Rank::Two });
+    }
+
+    #[test]
+    fn test_compare_flush_with_flush() {
+        let lhs = Combination::Flush { rank: Rank::Two };
+
+        assert_eq!(lhs, Combination::Flush { rank: Rank::Two });
+        assert_ne!(lhs, Combination::Flush { rank: Rank::Three });
+    }
+
+    #[test]
+    fn test_compare_full_house_with_different_combination() {
+        let lhs = Combination::FullHouse {
+            two: Rank::Two,
+            three: Rank::Three,
+        };
+
+        assert_ne!(lhs, Combination::HighCard { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four, Rank::Five]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::TwoPairs {
+                low: Rank::Two,
+                high: Rank::Three,
+                extra: [Rank::Four]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::ThreeOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four]
+            }
+        );
+        assert_ne!(lhs, Combination::Straight { rank: Rank::Two });
+        assert_ne!(lhs, Combination::Flush { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::FourOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three]
+            }
+        );
+        assert_ne!(lhs, Combination::StraightFlush { rank: Rank::Two });
+    }
+
+    #[test]
+    fn test_compare_full_house_with_full_house() {
+        let lhs = Combination::FullHouse {
+            two: Rank::Two,
+            three: Rank::Three,
+        };
+
+        assert_eq!(
+            lhs,
+            Combination::FullHouse {
+                two: Rank::Two,
+                three: Rank::Three
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::FullHouse {
+                two: Rank::Four,
+                three: Rank::Three
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::FullHouse {
+                two: Rank::Two,
+                three: Rank::Four
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::FullHouse {
+                two: Rank::Five,
+                three: Rank::Four
+            }
+        );
+    }
+
+    #[test]
+    fn test_compare_four_of_a_kind_with_different_combination() {
+        let lhs = Combination::FourOfAKind {
+            rank: Rank::Two,
+            extra: [Rank::Three],
+        };
+
+        assert_ne!(lhs, Combination::HighCard { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four, Rank::Five]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::TwoPairs {
+                low: Rank::Two,
+                high: Rank::Three,
+                extra: [Rank::Four]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::ThreeOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four]
+            }
+        );
+        assert_ne!(lhs, Combination::Straight { rank: Rank::Two });
+        assert_ne!(lhs, Combination::Flush { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::FullHouse {
+                two: Rank::Two,
+                three: Rank::Three
+            }
+        );
+        assert_ne!(lhs, Combination::StraightFlush { rank: Rank::Two });
+    }
+
+    #[test]
+    fn test_compare_four_of_a_kind_with_four_of_a_kind() {
+        let lhs = Combination::FourOfAKind {
+            rank: Rank::Three,
+            extra: [Rank::Four],
+        };
+
+        assert_eq!(
+            lhs,
+            Combination::FourOfAKind {
+                rank: Rank::Three,
+                extra: [Rank::Four],
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::FourOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Four],
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::FourOfAKind {
+                rank: Rank::Three,
+                extra: [Rank::Five],
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::FourOfAKind {
+                rank: Rank::Four,
+                extra: [Rank::Five],
+            }
+        );
+    }
+
+    #[test]
+    fn test_compare_straight_flush_with_different_combination() {
+        let lhs = Combination::StraightFlush { rank: Rank::Two };
+
+        assert_ne!(lhs, Combination::HighCard { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::Pair {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four, Rank::Five]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::TwoPairs {
+                low: Rank::Two,
+                high: Rank::Three,
+                extra: [Rank::Four]
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::ThreeOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three, Rank::Four]
+            }
+        );
+        assert_ne!(lhs, Combination::Straight { rank: Rank::Two });
+        assert_ne!(lhs, Combination::Flush { rank: Rank::Two });
+        assert_ne!(
+            lhs,
+            Combination::FullHouse {
+                two: Rank::Two,
+                three: Rank::Three
+            }
+        );
+        assert_ne!(
+            lhs,
+            Combination::FourOfAKind {
+                rank: Rank::Two,
+                extra: [Rank::Three]
+            }
+        );
+    }
+
+    #[test]
+    fn test_compare_straight_flush_with_straight_flush() {
+        let lhs = Combination::StraightFlush { rank: Rank::Two };
+
+        assert_eq!(lhs, Combination::StraightFlush { rank: Rank::Two });
+        assert_ne!(lhs, Combination::StraightFlush { rank: Rank::Three });
+    }
+}
